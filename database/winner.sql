a6488c53c64ad278774173c36ec1d207833402b9
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-03-06 12:39:19 +0100

    Update winner.sql

--- database/winner.sql
+++ database/winner.sql
@@ -1,7 +1,6 @@
-create table winner ( 
+CREATE TABLE winner ( 
 team_name VARCHAR(255), 
 tournaments_name VARCHAR(255),
 
-CONSTRAINT winner_primary 
-PRIMARY KEY(team_name, tournaments_name)
+CONSTRAINT winner_primary PRIMARY KEY(team_name, tournaments_name)
 )
