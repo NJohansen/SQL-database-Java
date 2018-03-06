79c4765ca7f997175c2a2f1eb8d4e1b0726b76de
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2018-03-06 12:36:12 +0100

    Update participants.sql

--- database/participants.sql
+++ database/participants.sql
@@ -1,7 +1,6 @@
-create table Participants ( 
-people_email VARCHAR(255), 
-tournaments_name VARCHAR(255),
+CREATE TABLE participants ( 
+  people_email VARCHAR(255), 
+  tournaments_name VARCHAR(255),
 
-CONSTRAINT participant_primary 
-PRIMARY KEY(people_email, tournaments_name)
+  CONSTRAINT participant_primary PRIMARY KEY(people_email, tournaments_name)
 )
