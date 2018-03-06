3d7cef1f5407921fd87c182bcb2f3d43e0e886a4
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-03-06 12:40:15 +0100

    Update people.sql

--- database/people.sql
+++ database/people.sql
@@ -1,6 +1,6 @@
 CREATE TABLE people(
-name VARCHAR(255),
-email VARCHAR(255) PRIMARY KEY, 
-nickname VARCHAR(255), 
+  name VARCHAR(255),
+  email VARCHAR(255) PRIMARY KEY, 
+  nickname VARCHAR(255), 
 )
 
