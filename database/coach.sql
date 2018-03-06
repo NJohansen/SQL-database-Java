4dff15c5f49b7bcd3f1222a24d765061745246b6
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2018-03-06 12:35:50 +0100

    Update coach.sql

--- database/coach.sql
+++ database/coach.sql
@@ -1,8 +1,7 @@
-create table coach ( 
-people_email VARCHAR(255), 
-team_name VARCHAR (255),
+CREATE TABLE coach ( 
+  people_email VARCHAR(255), 
+  team_name VARCHAR (255),
 
-CONSTRAINT coach_primary 
-PRIMARY KEY (people_email, team_name)
+  CONSTRAINT coach_primary PRIMARY KEY (people_email, team_name)
 ) 
 
