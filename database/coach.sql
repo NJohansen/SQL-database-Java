924d2ef952188a9d136b4183174b7bae8dfe3980
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-03-06 13:45:34 +0100

    Create team_member
    players og coach tabel samlet til én tabel

--- database/coach.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE coach ( 
-  people_email VARCHAR(255), 
-  team_name VARCHAR (255),
-
-  CONSTRAINT coach_primary PRIMARY KEY (people_email, team_name)
-) 
-
--- database/team_member.sql
+++ database/team_member.sql
@@ -1,6 +1,7 @@
-CREATE TABLE players ( 
+CREATE TABLE team_member( 
   people_email VARCHAR(255), 
-  team_name VARCHAR(255), 
+  team_name VARCHAR(255),
+  type VARCHAR(255),  
 
   CONSTRAINT player_primary PRIMARY KEY(people_email, team_name)
 )
