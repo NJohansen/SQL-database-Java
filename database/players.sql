3a10e6d257d3bd81a1d82eace14de64115dd4590
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2018-03-06 12:38:03 +0100

    Update players.sql

--- database/players.sql
+++ database/players.sql
@@ -1,7 +1,6 @@
-create table Players ( 
+CREATE TABLE players ( 
 people_email VARCHAR(255), 
 team_name VARCHAR(255), 
 
-CONSTRAINT player_primary
-PRIMARY KEY(people_email, team_name)
+CONSTRAINT player_primary PRIMARY KEY(people_email, team_name)
 )
